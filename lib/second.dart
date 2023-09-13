ca0d737224bb544d45da0439c0e0995030c6e88f
Eric Lee <ericlee@Erics-MacBook-Air.local>
2023-09-13 14:43:38 +0900

    initial commit

---
+++ lib/second.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SecondView extends StatefulWidget {
+  const SecondView({super.key});
+
+  @override
+  State<SecondView> createState() => _SecondViewState();
+}
+
+class _SecondViewState extends State<SecondView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second View Text"),
+        // leading: IconButton(
+        //   icon: const Icon(Icons.arrow_back),
+        //   onPressed: () => Navigator.of(context).pop(),
+        // ),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          color: Colors.blue,
+          child: const Text("this is second view."),
+        ),
+      ),
+    );
+  }
+}
+
+
